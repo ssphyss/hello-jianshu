25fd4293057dd411f52fa372e4e51557c440fab6
ssphyss <ss.physs@gmail.com>
2018-10-23 21:02:50 +0800

    commit branch12 首頁Topic組件

--- src/components/style.js
+++ src/components/style.js
@@ -1,23 +1,37 @@
 import styled from 'styled-components';
 
-export const HomeWrapper = styled.div`
+export const TopicWrapper = styled.div`
     overflow: hidden;
-    width: 960px;
-    margin: 0 auto;
+    padding: 20px 0 10px 0;
+    margin-left: -18px;
+    border: 1px solid;
 `;
 
-export const HomeLeft = styled.div`
+export const TopicItem = styled.div`
     float: left;
-    margin-left: 15px;
-    padding-top: 30px;
-    width: 625px;
-    .banner-img {
-        width: 625px;
-        height: 270px;
+    margin-left: 18px;
+    margin-bottom: 18px;
+    padding-right: 10px;
+    height: 32px;
+    line-height: 32px;
+    font-size: 14px;
+    color: #777;
+    border: 1px solid #dcdcdc;
+    border-radius: 4px;
+    .topic-pic {
+        margin-right: 10px;        
+        display: blick;
+        float: left;
+        width: 32px;
+        height: 32px;  
     }
 `;
 
-export const HomeRight = styled.div`
-    float: right;
-    width: 240px;
+export const More = styled.a`
+    display: inline-block;
+    margin-left: 18px;
+    font-size: 12px;
+    height: 32px;
+    line-height: 32px;
+    color: #999;
 `;
